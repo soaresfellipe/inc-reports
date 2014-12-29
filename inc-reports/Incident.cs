ed6f367afffb463e574ccf26977366cf60b88fa4
Fellipe Soares <soaresfellipe@outlook.com>
2014-12-29 10:43:24 -0200

    Commit to add project configurations and class Incident

---
+++ inc-reports/Incident.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inc_reports
+{
+    public class Incident
+    {
+        private int id;
+        private String incidentNumber;
+        private char severity;
+        private String assignee;
+        private String assignement;
+        private String status;
+        private DateTime openTime;
+        private DateTime updateTime;
+        private DateTime resolvedTime;
+        private DateTime closeTime;
+        private DateTime reopenTime;
+        private bool slaBreached;
+        private String vendorTicket;
+    }
+}
