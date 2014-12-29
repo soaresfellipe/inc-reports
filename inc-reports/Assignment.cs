970fa77cba96f8fd53fc92e9ecb3ed4b9db219ad
Fellipe Soares <soaresfellipe@outlook.com>
2014-12-29 15:27:21 -0200

    [UPD] Assignment.cs - Include property Description

---
+++ inc-reports/Assignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inc_reports
+{
+    class Assignment
+    {
+        private int id;
+        private string name;
+        private string description;
+
+        // Getters and Setters
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
