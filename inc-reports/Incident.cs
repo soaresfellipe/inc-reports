dd7d4fc76105db199312be6653606a08335e3d4e
Fellipe Soares <soaresfellipe@outlook.com>
2014-12-29 14:56:48 -0200

    [UPD] Incident.cs - Generate Getters and Setters

--- inc-reports/Incident.cs
+++ inc-reports/Incident.cs
@@ -9,17 +9,33 @@ namespace inc_reports
     public class Incident
     {
         private int id;
-        private String incidentNumber;
+        private string incidentNumber;
         private char severity;
-        private String assignee;
-        private String assignement;
-        private String status;
+        private string assignee;
+        private string assignement;
+        private string status;
         private DateTime openTime;
         private DateTime updateTime;
-        private DateTime resolvedTime;
+        private DateTime resolveTime;
         private DateTime closeTime;
         private DateTime reopenTime;
         private bool slaBreached;
-        private String vendorTicket;
+        private string vendorTicket;
+
+        // Getters and Setters
+        public int id { get; set; }
+        public string incidentNumber { get; set; }
+        public char severity { get; set; }
+        public string assignee { get; set; }
+        public string assignement { get; set; }
+        public string status { get; set; }
+        public DateTime openTime { get; set; }
+        public DateTime updateTime { get; set; }
+        public DateTime resolveTime { get; set; }
+        public DateTime closeTime { get; set; }
+        public DateTime reopenTime { get; set; }
+        public bool slaBreached { get; set; }
+        public string vendorTicket { get; set; }
+
     }
 }
